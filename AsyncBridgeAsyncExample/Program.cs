9e03f878d7600d7a34b8d484741e4e8f9fd9b220
jun maeda <j.maeda@activeltd.co.jp>
2018-10-16 14:21:06 +0900

    テストの修正。 Theraot.Core での Task.RunのCPUが無駄に食う問題がわかる形でサンプル実装

--- AsyncBridgeAsyncExample/Program.cs
+++ AsyncBridgeAsyncExample/Program.cs
@@ -10,7 +10,7 @@ namespace AsyncBridgeAsyncExample
 {
     class Program
     {
-        static readonly TimeSpan WaitTime = TimeSpan.FromSeconds(1);
+        static readonly TimeSpan WaitTime = TimeSpan.FromSeconds(20);
         static async Task Main(string[] args)
         {
             var listener = new TextWriterTraceListener(Console.Out);
@@ -21,27 +21,26 @@ namespace AsyncBridgeAsyncExample
                 TokenSource?.Cancel();
                 return false;
             }))
-                await MainRoop(TokenSource.Token);
+                await MainAction(TokenSource.Token);
             
         }
-        static async Task MainRoop(CancellationToken Token)
+        static async Task MainAction(CancellationToken Token)
         {
-            uint Number = 0;
-            var FriendlyName = System.AppDomain.CurrentDomain.FriendlyName;
-            Trace.WriteLine($"{FriendlyName} Start");
-            while (!Token.IsCancellationRequested)
-            {
-                try
-                {
-                    Trace.WriteLine($"{FriendlyName} {nameof(WaitTime)} {Number++}");
-                    await TaskEx.Delay(WaitTime, Token);
-                }
-                catch (OperationCanceledException)
-                {
-                    Trace.WriteLine($"{FriendlyName} Cancel");
-                }
-            }
-            Trace.WriteLine($"{FriendlyName} End");
+            DateTime StartTime = DateTime.Now;
+            await Task.Factory.StartNew(() => Task2Action(StartTime, Token), Token).Unwrap();
+            Trace.WriteLine($"Task1 {DateTime.Now - StartTime}");
+            await TaskEx.Run(() => Task4Action(StartTime, Token), Token);
+            Trace.WriteLine($"Task3 {DateTime.Now - StartTime}");
+        }
+        static async Task Task2Action(DateTime StartTime, CancellationToken Token)
+        {
+            await TaskEx.Delay(WaitTime, Token);
+            Trace.WriteLine($"Task2 {DateTime.Now - StartTime}");
+        }
+        static async Task Task4Action(DateTime StartTime, CancellationToken Token)
+        {
+            await TaskEx.Delay(WaitTime, Token);
+            Trace.WriteLine($"Task4 {DateTime.Now - StartTime}");
         }
     }
 }
